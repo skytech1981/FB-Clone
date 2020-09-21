c3ec3b4ab1a4060d423258c75f0f899c298115f3
sali itzhaki <sali1943@hotmail.com>
2020-09-21 15:42:43 +0300

    stating facebook from scratch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
- 
 import './App.css';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
 
 function App() {
   return (
     // 
     <div className="app">
-     <h1>Facebook clone</h1>
+     
+     <Header/>
+     <div className="app__body">
+       <Sidebar/>
+       {/* sidebar */}
+       {/* feed */}
+       {/* widget */}
+     </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+          <SidebarRow title='Pages'/>  
+          <SidebarRow title ='Friends'/>  
+           
+        </div>
+    )
+}
+
+export default Sidebar
