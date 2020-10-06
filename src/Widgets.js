ec5f98c5d0e26cf51c4f5ba29b65a41077f72479
sali itzhaki <sali1943@hotmail.com>
2020-10-06 19:22:22 +0300

    save improvment

--- src/Widgets.js
+++ src/Widgets.js
@@ -5,18 +5,18 @@ import "./Widgets.css"
 function Widgets() {
     return (
         <div className="widgets">
-            {/* <iframe
-            src=""
+            <iframe
+            src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook%2Fvideos%2F10153231379946729%2F&show_text=false&width=734&height=411&appId"
             width="340"
             height="100%"
-            style={{border:"none" ,overflow="hidden"}}
+            style={{border:"none" ,overflow:"hidden"}}
             scrolling="none"
             frameborder="0"
             allowTransparency="true"
-            allow="encrypted-media"
+            allow="encrypted-media">
 
 
-            </iframe> */}
+            </iframe>
         </div>
     )
 }
